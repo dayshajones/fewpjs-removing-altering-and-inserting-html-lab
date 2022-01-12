db7fcccf17487aaaa82018ed2461aa6379001b02
“Daysha <+>
2022-01-12 12:53:29 -0800

    commit tree a0eef28c615d8d5f0952d1d313336a43000e80f2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+//has a 'newHeader' variable that points to node 'h1#victory'
+let newHeader = document.createElement("h1");
+ newHeader.setAttribute("id","victory");
+
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "YOUR - NAME is the champion"; 
